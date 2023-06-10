79e9144ba1ba30c48aa99a2a932583ef9a716477
Chris Eun <eunchristopher@gmail.com>
2023-06-10 10:51:45 -0700

    chris merge dev

---
+++ src/frontend/components/Footer.jsx
@@ -0,0 +1,148 @@
+import { MailOutline } from "@material-ui/icons";
+import { Facebook, FmdGood, Instagram, Phone, Pinterest, Twitter } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  display: flex;
+  margin: 20px 15px;
+`
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+
+const Logo = styled.h1`
+
+`
+
+const Description = styled.div`
+  margin: 20px 0;
+  line-height: 1.75rem;
+`
+
+const SocialContainer = styled.div`
+  display: flex;
+`
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #${props => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+
+`
+
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`
+
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`
+
+const Payment= styled.img`
+  width: 50%;
+`
+
+
+const Footer = ({item}) => {
+  return (
+      <Container>
+        <Left>
+          <Logo>Marlons</Logo>
+          <Description>where style meets comfort,
+            offers a wide range of trendy and high-quality clothing for men and women,
+            perfect for every occasion with Marlons
+          </Description>
+          <SocialContainer>
+            <SocialIcon color="3B5999">
+              <Facebook />
+            </SocialIcon>
+            <SocialIcon color="E4405F">
+              <Instagram />
+            </SocialIcon>
+            <SocialIcon color="55ACEE">
+              <Twitter />
+            </SocialIcon>
+            <SocialIcon color="E60023">
+              <Pinterest />
+            </SocialIcon>
+          </SocialContainer>
+        </Left>
+        <Center>
+          <Title>
+            Useful Links
+          </Title>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Men Fashion</ListItem>
+            <ListItem>Women Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+        </Center>
+        <Right>
+          <Title>
+            Contact
+          </Title>
+          <ContactItem>
+            <FmdGood style={{marginRight: "10px"}}/>
+            1901 E Second St., Santa Ana 92705
+          </ContactItem>
+          <ContactItem>
+            <Phone style={{marginRight: "10px"}} />
+            +1 594 302 4293
+          </ContactItem>
+          <ContactItem>
+            <MailOutline style={{marginRight: "10px"}} />
+            contactandshop@marlons.dev
+          </ContactItem>
+          <Payment src={require("../images/card-page.jpeg")} />
+        </Right>
+
+      </Container>
+  );
+}
+
+export default Footer;
---
+++ src/frontend/data.js
@@ -0,0 +1,98 @@
+import mainPageImg from "./images/main-page.jpg"
+import mainPageImg1 from "./images/main-page-1.jpg"
+import mainPageImg2 from "./images/main-page-2.jpg"
+import catPageImg from "./images/cat-page.jpg"
+import catPageImg1 from "./images/cat-page-1.jpg"
+import catPageImg2 from "./images/cat-page-2.jpg"
+import popPage from "./images/pop-page.jpg"
+import popPage1 from "./images/pop-page-1.jpg"
+import popPage2 from "./images/pop-page-2.jpg"
+import popPage3 from "./images/pop-page-3.jpg"
+import popPage4 from "./images/pop-page-4.jpg"
+import popPage5 from "./images/pop-page-5.jpg"
+import popPage6 from "./images/pop-page-6.jpg"
+import popPage7 from "./images/pop-page-7.jpg"
+import cardPage from "./images/card-page.jpeg"
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: mainPageImg,
+    title: "SUMMER SALE",
+    desc: "20% OFF COUPON CODE FOR NEW CUSTOMERS",
+    alt: "Main-Summer-Page-Image",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: mainPageImg1,
+    title: "NEW ARRIVALS",
+    desc: "Free Shipping on Orders Over $60",
+    alt: "New-Arrival-Page-Image",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: mainPageImg2,
+    title: "BEST SELLERS",
+    desc: "FOLLOW YOUR FASHION CREATIVITY",
+    alt: "Best-Seller-Page-Image",
+    bg: "fbf0f4",
+  },
+]
+
+export const categories = [
+  {
+    id: 1,
+    img: catPageImg,
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 2,
+    img: catPageImg1,
+    title: "SEASONAL",
+
+  },
+  {
+    id: 3,
+    img: catPageImg2,
+    title: "SIMPLE STYLE",
+
+  },
+]
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: popPage,
+  },
+  {
+    id: 2,
+    img: popPage1,
+  },
+  {
+    id: 3,
+    img: popPage2,
+  },
+  {
+    id: 4,
+    img: popPage3,
+  },
+  {
+    id: 5,
+    img: popPage4,
+  },
+  {
+    id: 6,
+    img: popPage5,
+  },
+  {
+    id: 7,
+    img: popPage6,
+  },
+  {
+    id: 8,
+    img: popPage7,
+  },
+
+]
