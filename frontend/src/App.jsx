c94731da8cd29933504f6830ab7d40c91962e1a6
Marlon Baker <marbaker.swe@gmail.com>
2023-06-12 14:46:13 -0700

    added backend databe reorg frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,49 @@
+
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Product from './pages/Product';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+
+const App = () => {
+
+const [backendData, setBackendData] = useState([{}]);
+
+useEffect(() => {
+  fetch('/api').then(
+    response => response.json()
+  ).then(
+    data => {
+    setBackendData(data);
+    }
+  )
+}, []);
+
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+      <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+       <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<ProductList />} />
+        <Route path="products/:productId" element={<Product />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+    </div>
+    </div>
+  );
+
+}
+
+export default App;
