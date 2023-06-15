d8efdcc6df0c92ecf54d11f304e28009f435fd89
Marlon Baker <marbaker.swe@gmail.com>
2023-06-14 21:49:00 -0700

    update app.js

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,33 +1,24 @@
-import Product from "./components/pages/Product";
-import Home from "./components/pages/Home";
-import ProductList from "./components/pages/ProductList";
-import Register from "./components/pages/Register";
-import Login from "./components/pages/Login";
-import Cart from "./components/pages/Cart";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Product from './components/pages/Product';
+import Home from './components/pages/Home';
+import ProductList from './components/pages/ProductList';
+import Register from './components/pages/Register';
+import Login from './components/pages/Login';
+import Cart from './components/pages/Cart';
 
 const App = () => {
-  const user = true
+  const user = true;
   return (
-   <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route
-          path="/register"
-          element={user ? <Navigate to="/" /> : <Register />}
-        />
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </Router>
-    )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/register"element={user ? <Navigate to="/" /> : <Register />}/>
+      <Route path="/login" element={user ? <Navigate to="/login" /> : <Login />}/>
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+};
 
 export default App;
