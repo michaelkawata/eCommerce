60b856d7c6d838d406009853b4e15f5eee4ad444
Chris Eun <eunchristopher@gmail.com>
2023-06-19 10:24:42 -0700

    update page

--- src/frontend/data.js
+++ src/frontend/data.js
@@ -1,9 +1,9 @@
-import mainPageImg from "./assets/images/main-page.jpg"
-import mainPageImg1 from "./assets/images/main-page-1.jpg"
-import mainPageImg2 from "./assets/images/main-page-2.jpg"
-import catPageImg from "./assets/images/cat-page.jpg"
-import catPageImg1 from "./assets/images/cat-page-1.jpg"
-import catPageImg2 from "./assets/images/cat-page-2.jpg"
+// import mainPageImg from "./images/main-page.jpg"
+// import mainPageImg1 from "./images/main-page-1.jpg"
+// import mainPageImg2 from "./images/main-page-2.jpg"
+// import catPageImg from "./images/cat-page.jpg"
+// import catPageImg1 from "./images/cat-page-1.jpg"
+// import catPageImg2 from "./images/cat-page-2.jpg"
 import popPage from "./assets/images/pop-page.jpg"
 import popPage1 from "./assets/images/pop-page-1.jpg"
 import popPage2 from "./assets/images/pop-page-2.jpg"
@@ -12,51 +12,53 @@ import popPage4 from "./assets/images/pop-page-4.jpg"
 import popPage5 from "./assets/images/pop-page-5.jpg"
 import popPage6 from "./assets/images/pop-page-6.jpg"
 import popPage7 from "./assets/images/pop-page-7.jpg"
+// import cardPage from "./images/card-page.jpeg"
 
 export const sliderItems = [
   {
     id: 1,
-    img: mainPageImg,
+    img: "https://ecommercemcm.s3.us-west-1.amazonaws.com/marbaker.swe_black_young_women_wearing_color_japanese_desiner_s_a6ddb084-b7e1-4201-8309-c26444917b16.png",
     title: "SUMMER SALE",
     desc: "20% OFF COUPON CODE FOR NEW CUSTOMERS",
     alt: "Main-Summer-Page-Image",
-    bg: "f5fafd",
+    bg: "FFF4EE",
   },
   {
     id: 2,
-    img: mainPageImg1,
+    img: "https://ecommercemcm.s3.us-west-1.amazonaws.com/4/marbaker.swe_black_young_women_wearing_japanese_desiner_shirt_h_c7eb5b5e-74a2-45d6-a284-6191b39babcb.png",
     title: "NEW ARRIVALS",
     desc: "Free Shipping on Orders Over $60",
     alt: "New-Arrival-Page-Image",
-    bg: "fcf1ed",
+    bg: "fbf0f4",
   },
   {
     id: 3,
-    img: mainPageImg2,
+    img: "https://ecommercemcm.s3.us-west-1.amazonaws.com/women/marbaker.swe_white_young_women_wearing_asian_desiner_shirt_hip__9b89175c-0a53-42f8-b06d-84f9512e7e63.png",
     title: "BEST SELLERS",
     desc: "FOLLOW YOUR FASHION CREATIVITY",
-    alt: "Best-Seller-Page-Image",
-    bg: "fbf0f4",
+    alt: "https://ecommercemcm.s3.us-west-1.amazonaws.com/Jeans/marbaker.swe_japanese_desiner_jeans_only_white_background_hip_h_ca2cf2ee-3574-4ea4-91fb-09e5330a0627+copy3.png",
+    bg: "EBE2DF",
   },
 ]
 
 export const categories = [
   {
     id: 1,
-    img: catPageImg,
-    title: "SHIRT STYLE",
+    img: "https://ecommercemcm.s3.us-west-1.amazonaws.com/women/marbaker.swe_black_young_women_wearing_japanese_desiner_shirt_h_c7eb5b5e-74a2-45d6-a284-6191b39babcb+copy3.png",
+    title: "WOMEN",
+    cat: "women"
   },
   {
     id: 2,
-    img: catPageImg1,
-    title: "SEASONAL",
-
+    img: "https://ecommercemcm.s3.us-west-1.amazonaws.com/Jeans/marbaker.swe_japanese_desiner_jeans_only_white_background_hip_h_11fbdee7-c088-4f64-9f5c-0830f84f15ba+copy1.png",
+    title: "JEANS",
+    cat: "jeans"
   },
   {
     id: 3,
-    img: catPageImg2,
-    title: "SIMPLE STYLE",
-
+    img: "https://ecommercemcm.s3.us-west-1.amazonaws.com/other/marbaker.swe_black_young_man_wearing_asian_desiner_shirt_high_f_338b3137-c744-4f1b-b7dc-99963feb5b5f+copy3.png",
+    title: "MEN",
+    cat: "men"
   },
 ]
 
