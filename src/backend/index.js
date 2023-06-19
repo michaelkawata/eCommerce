d3c713bd12afaa215713e3bea018653690f1be89
Chris Eun <eunchristopher@gmail.com>
2023-06-19 10:13:47 -0700

    backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,43 +1,41 @@
-require('dotenv').config()
-const express = require("express");
+const express = require("express")
 const app = express();
-const mongoose = require("mongoose");
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const productRoute = require("./routes/products");
-const cartRoute = require("./routes/cart");
-const cors = require("cors");
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
-const path = require('path')
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
-app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
 
-// MongoDB connection
+const cors= require("cors")
+
+
+
+
+dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
+  .then(() => console.log("DB Connection Successful!"))
   .catch((err) => {
-    console.log(err);
+    console.log(err)
   });
 
-app.use(cors());
-app.use(express.json());
-app.use("/auth", authRoute);
-app.use("/users", userRoute);
-app.use("/products", productRoute);
-app.use("/carts", cartRoute);
-
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true })); // new code from stackoverflow
-app.use(express.json()); // new code from stackoverflow
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', '..', 'dist', "index.html"));
-});
+  app.use(cors())
+  app.use(express.json())
+  app.use("/api/auth", authRoute)
+  app.use("/api/users", userRoute)
+  app.use("/api/products", productRoute)
+  app.use("/api/carts", cartRoute)
+  app.use("/api/orders", orderRoute)
+  app.use("/api/checkout", stripeRoute)
 
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+app.listen(process.env.port || 5000, () => {
+  console.log("Backend server is running!")
+})
--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -1,10 +1,125 @@
 const router = require("express").Router();
-const authController = require("../controllers/authController")
+const User = require("../models/User")
+const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
-// Route for registering a new user, handled by the register method of authController
-router.post("/register", authController.register)
+//Register
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    //replaced from "req.body.password"
+    //PASS_SEC from .env
+    //encrypting password
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  })
 
-// Route for logging in a user, handled by the login method of authController
-router.post("/login", authController.login)
+  try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+//LOGIN
+
+// router.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
+
+//   console.log('Request received with:', req.body);
+
+//   try {
+//     // Find the user by username
+//     const user = await User.findOne({ username });
+
+//     console.log('User found:', user);
+
+//     if (!user) {
+//       return res.status(401).json({ message: 'Invalid username or password.' });
+//     }
+
+//     // Compare the inputted password with the hashed password
+//     const passwordMatch = await bcrypt.compare(password, user.password);
+
+//     console.log('Passwords match:', passwordMatch);
+
+//     if (!passwordMatch) {
+//       return res.status(401).json({ message: 'Invalid username or password.' });
+//     }
+
+//     console.log('JWT Secret:', process.env.JWT_SEC);
+
+//     //JWT - JSON WEB TOKEN
+//     const accessToken = jwt.sign({
+//       id: user._id,
+//       isAdmin: user.isAdmin,
+//     }, process.env.JWT_SEC, {
+//       expiresIn: "3d"
+//     })
+
+//     console.log('Access token:', accessToken);
+
+//     //showing everything but password in mongodb
+//     const {
+//       password: userPassword,
+//       ...others
+//     } = user._doc;
+
+//     console.log('Response data:', {...others, accessToken});
+
+//     return res.status(200).json({...others, accessToken});
+
+//   } catch (err) {
+//     console.log('Error:', err);
+//     return res.status(500).json(err);
+//   }
+// });
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+        username: req.body.username
+      })
+
+      !user && res.status(401).json("Username or Password is not correct!")
+
+    //decrypting password
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password, process.env.PASS_SEC
+    )
+
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+    OriginalPassword !== req.body.password && res.status(401).json("Username or Password is not correct!")
+
+    //JWT - JSON WEB TOKEN
+    const accessToken = jwt.sign({
+      id: user._id,
+      isAdmin: user.isAdmin,
+    }, process.env.JWT_SEC, {
+      //JWT expires in 3 days and user will have to login again
+      expiresIn: "3d"
+    })
+
+    //showing everything but password in mongodb
+    //._doc is used only for mongo
+    const {
+      password,
+      ...others
+    } = user._doc;
+
+    res.status(200).json({...others, accessToken})
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+
+})
+
+
 
 module.exports = router
