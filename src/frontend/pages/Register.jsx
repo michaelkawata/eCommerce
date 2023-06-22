6719412b4608fa906b6ab42979f714732698c036
michaelkawata <mikeykawata@gmail.com>
2023-06-21 17:46:08 -0700

    register

--- src/frontend/pages/Register.jsx
+++ src/frontend/pages/Register.jsx
@@ -73,7 +73,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("/api/auth/register", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
